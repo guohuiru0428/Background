080e4ec2ee99f67e8f2d6b79abbda9c669f2d898
郭慧茹 <7873191+guo_huiru@user.noreply.gitee.com>
2020-09-03 21:35:37 +0800

    登录功能

---
+++ src/components/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+     我是布局页面
+ </div>
+</template>
+
+<script>
+export default {
+ name : "", 
+ data(){
+  return {
+  }
+ },
+ components : {
+ },
+}
+</script>
+
+
+<style scoped>
+</style>
